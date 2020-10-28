4adb5364013c745357056eb3dcd8546f2cca41ec
Thomas Pimentel <thomas@webscaping.ca>
2020-10-28 01:24:29 -0400

    Completed Direction model & unit tests

---
+++ backend/spec/factories/directions.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+    factory :direction do
+        factory :direction_one do
+            direction   { "Bring potatoes to a boil then strain and mash, mash, mash!!!!" }
+            seq         { 1 }
+            association :recipe
+        end
+    
+        factory :direction_two do
+            direction   { "In another pot boil Kale and add to mased potatoes with water" }
+            seq         { 2 }
+            association :recipe
+        end
+    end
+end
