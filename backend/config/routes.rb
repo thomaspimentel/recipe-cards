b113f84d61dc717fd4d854e82d8dbab11460c333
Thomas Pimentel <Thomas@webscaping.ca>
2021-07-18 10:55:20 -0400

    rails upgrade to 6.1

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/graphql', to: 'graphql#execute'
+
   namespace :api do
     namespace :dev do
       resources :recipes
